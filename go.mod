66cbc805ee6ee2e559dae2e4e4c8d3d93d8b8087
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-02-17 23:33:37 -0800

    Create http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/agent-e11/pagination_go
 
 go 1.21
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
 
 	dbc "github.com/agent-e11/pagination_go/dbcontrol"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-    var quantity int = 5
-    for i := 0; i < 10; i++ {
-        fmt.Printf("Page %d: %v\n", i, dbc.GetPage(dbc.DB, i, quantity))
+    //var quantity int = 5
+    //for i := 0; i < 10; i++ {
+    //    fmt.Printf("Page %d: %v\n", i, dbc.GetPage(dbc.DB, i, quantity))
+    //}
+
+    router := httprouter.New()
+
+    router.GET("/:page", Index)
+
+    log.Print("Running http server...")
+    http.ListenAndServe(":8000", router)
+}
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    tmpl := template.Must(template.ParseFiles("index.tmpl.html"))
+
+    pageNum, err := strconv.Atoi(ps.ByName("page"))
+    if err != nil {
+        w.WriteHeader(404)
     }
+    
+    data := map[string]any{
+        "Items": dbc.GetPage(dbc.DB, pageNum, 5),
+        "Page": pageNum,
+        "NextPage": pageNum + 1,
+        "PrevPage": pageNum - 1,
+    }
+
+    tmpl.Execute(w, data)
 }
